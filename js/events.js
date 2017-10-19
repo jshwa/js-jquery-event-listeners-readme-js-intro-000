61a61e8418955240bf8b7e38c98ba14ada2ff54f
Joshua Palacios <palacios.joshua@gmail.com>
2017-10-19 22:25:27 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,14 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    return 'Hey!'
+  })
+}
+
 
 $(document).ready(function(){
 
+getIt()
 // call functions here
 
 });
